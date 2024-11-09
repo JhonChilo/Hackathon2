1620548cea33d55bb5adba798fde9c6819a066b4
JhonChilo <jhon.chilo@utec.edu.pe>
2024-11-09 15:50:10 -0500

    Plantilla

---
+++ Hackathon2/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css';
+
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-500">
+        ¡Hola, Hackathon 2!
+      </h1>
+    </div>
+  );
+}
+
